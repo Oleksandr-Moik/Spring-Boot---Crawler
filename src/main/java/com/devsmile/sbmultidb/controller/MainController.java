08b3c13bd594b22084a4ee8aa4b5f5dc08e26dbf
Virus_Mix <sashamoik2016@gmail.com>
2019-08-29 16:22:16 +0300

    1 rev

--- src/main/java/com/devsmile/sbmultidb/controller/MainController.java
+++ src/main/java/com/devsmile/sbmultidb/controller/MainController.java
@@ -2,21 +2,22 @@ package com.devsmile.sbmultidb.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsmile.sbmultidb.crawler.WebCrawler;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class MainController {
 
-    @Autowired
     private WebCrawler webCrawler;
     
     @GetMapping(value = "/")
-    public Map<String, String> showCategoties() throws Exception {
+    public Map<String, String> getCategoties() throws Exception {
         return webCrawler.getCategoties();
     }
     
--- src/main/java/com/devsmile/sbmultidb/crawler/WebCrawler.java
+++ src/main/java/com/devsmile/sbmultidb/crawler/WebCrawler.java
@@ -1,94 +1,77 @@
 package com.devsmile.sbmultidb.crawler;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class WebCrawler {
 
-    private static final String LINK_AIN = "https://ain.ua";
+    @Value("${url}")
+    private String LINK_AIN;
 
-    private Map<String, String> categoriesMAP;
-    private Map<String, String> articlesMAP;
-    private HashSet<String> pageLinksSET;
+    private Map<String, String> linksMap = new HashMap<>();
+    private Set<String> pageLinksSET;
     private String currentTagCategory;
 
-    public WebCrawler() {
-        categoriesMAP = new HashMap<String, String>();
-        articlesMAP = new HashMap<String, String>();
-        pageLinksSET = new HashSet<String>();
-    }
-
-    public Map<String, String> getCategoties() {
-        try {
+    public Map<String, String> getCategoties() throws Exception {
+        
             Document document = Jsoup.connect(LINK_AIN).get();
             Elements elem = document.select("div[class=tags-block web-view]").select("a[class=look-all]");
-            // Elements elem = document.select("div[class=tags-block web-view]
-            // a[class=look-all]");
+            linksMap.clear();
             for (Element el : elem) {
                 String url = el.attr("href");
-                if (categoriesMAP.put(getTitle(url),url) != null)
-                    System.out.println("Category: " + getTitle(url) + " || " + url);
+                if(url.contains("tag"))
+                    if (linksMap.put(getTitle(url),url) != null){
+                        System.out.println("Category: " + getTitle(url));
+                        System.out.println("http://localhost:8080"+url);
+                    }
             }
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-        return categoriesMAP;
+
+            return linksMap;
     }
 
-    private String getTitle(String url) {
-        try {
-            if (!url.contains("ain.ua"))
-                return Jsoup.connect(LINK_AIN + url).get().title();
-            else
-                return Jsoup.connect(url).get().title();
-        } catch (Exception e) {
-            return null;
-        }
+    private String getTitle(String url) throws Exception{
+        if (!url.contains("ain.ua"))
+            return Jsoup.connect(LINK_AIN + url).get().title();
+        else
+            return Jsoup.connect(url).get().title();
     }
 
-    public Map<String, String> getArticles() {
+    public Map<String, String> getArticles() throws Exception{
         getPagesLinks(LINK_AIN + currentTagCategory);
-        pageLinksSET.forEach(link -> {
-            try {
-                Document document = Jsoup.connect(link).get();
-                Elements articleLinks = document.select("a[class^=\"post-link\"]");
-                for (Element article : articleLinks) {
-                    articlesMAP.put(article.text(), article.attr("abs:href"));
-                    System.out.println(article.text().length() + "  " + article.attr("abs:href"));
-                }
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
+        linksMap.clear();
+        for(String link:pageLinksSET) {
+            Document document = Jsoup.connect(link).get();
+            Elements articleLinks = document.select("a[class^=\"post-link\"]");
+            for (Element article : articleLinks) {
+                linksMap.put(article.text(), article.attr("abs:href"));
+                System.out.println(article.text().length() + "  " + article.attr("abs:href"));
             }
-        });
-
-        return articlesMAP;
+        }
+        return linksMap;
     }
 
     public void setCategory(String category) {
         this.currentTagCategory = "/tag/" + category;
     }
 
-    private void getPagesLinks(String url) {
+    private void getPagesLinks(String url) throws Exception{
         if (!pageLinksSET.contains(url)) {
-            try {
-                Document document = Jsoup.connect(url).get();
-                Elements pageLinks = document.select("a[href^=\"" + LINK_AIN + currentTagCategory + "/page/\"]");
-                for (Element link : pageLinks) {
-                    if (pageLinksSET.add(url)) {
-                        System.out.println(url);
-                    }
-                    getPagesLinks(link.attr("abs:href"));
+            Document document = Jsoup.connect(url).get();
+            Elements pageLinks = document.select("a[href^=\"" + LINK_AIN + currentTagCategory + "/page/\"]");
+            for (Element link : pageLinks) {
+                if (pageLinksSET.add(url)) {
+                    System.out.println(url);
                 }
-            } catch (Exception e) {
-                System.err.println("Bad url? for " + url + ": category not found " + e.getMessage());
+                getPagesLinks(link.attr("abs:href"));
             }
         }
     }
