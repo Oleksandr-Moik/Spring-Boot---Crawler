c0383790703a99fd3bd8a7b031ddc5d60f67431e
Virus_Mix <sashamoik2016@gmail.com>
2019-09-26 13:49:00 +0300

    xpk

--- src/main/java/com/devsmile/sbmultidb/controller/CrawlerController.java
+++ src/main/java/com/devsmile/sbmultidb/controller/CrawlerController.java
@@ -1,6 +1,8 @@
+
 package com.devsmile.sbmultidb.controller;
 
 import java.util.Map;
+import java.util.Random;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,5 +26,39 @@ public class CrawlerController {
         log.info("Call getPage with params: category = {}, page = {}", category, page);
 
         return crawlerService.getLinks(category, page);
+    }
+    
+    @GetMapping("/sort/{count}")
+    public String bubbleSort(@PathVariable Integer count) {
+        String result="";
+        
+        int n=count;
+        int mas[] = new int[n];
+        result += "Input array: ";
+        Random rand=new Random();
+        for (int i = 0; i < n; i++)
+        {
+          mas[i] = rand.nextInt(20);
+          result+= mas[i] + " ";
+        }
+        int k=0;
+        for (int i = 0; i < n; i++) {
+          for (int j = i+1; j < n; j++) {
+            if (mas[i] > mas[j]) {
+              k=mas[i];
+              mas[i]=mas[j];
+              mas[j]=k;
+            }
+            k++;
+          }
+        }
+//        result+="<br>k"+k;
+        result+="<br>Sorted array: ";
+        for (int i = 0; i < n; i++)
+        {
+            result+=mas[i] + " ";
+        }
+        
+        return result;
     }
 }
